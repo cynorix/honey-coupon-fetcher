4b97c2a8c17a61526e0e6cb6664893fbc4e2b0b9
cynorix <65047887+cynorix@users.noreply.github.com>
2025-01-07 23:07:46 +0000

    Fixed "403 forbidden" error

--- fetcher.py
+++ fetcher.py
@@ -7,7 +7,10 @@ params = {
     'operationName': 'ext_getStorePartialsByDomain',
     'variables': '{"domain":"' +  site + '"}'
 }
-r = requests.get(url=f'https://d.joinhoney.com/v3', params=params)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0'
+}
+r = requests.get(url=f'https://d.joinhoney.com/v3', params=params, headers=headers)
 # end of request to get site ID from URL
 if "storeId" in r.text:
     storeid = r.json()['data']['getPartialURLsByDomain'][0]['storeId']
@@ -15,12 +18,13 @@ if "storeId" in r.text:
     params = {
         'coupons': '1'
     }
-    r = requests.get(url=f"https://d.joinhoney.com/stores/{storeid}?coupons=1")
+    r = requests.get(url=f"https://d.joinhoney.com/stores/{storeid}?coupons=1", headers=headers)
     # end of request to get coupons for site
     coupons = r.json()['coupons']
     if len(coupons) < 1:
         print('no coupons found!')
-    for coupon in coupons:
-        print(coupon['code'])
+    else:
+        for coupon in coupons:
+            print(coupon['code'])
 else:
     print("Honey doesn't know this site! Are you sure you typed it in correctly?")
